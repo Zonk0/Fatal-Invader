e386eac6ac968694dd4b4811aa5648a4ef8578cc
Zonk0 <74017319+Zonk0@users.noreply.github.com>
2021-03-26 23:53:35 +0000

    Error message
    Hello from the pygame community. https://www.pygame.org/contribute.html
    Traceback (most recent call last):
      File "c:/Users/ala20/Desktop/programming nea/bullet hell/bu.py", line 370, in <module>
        draw_health(screen, WIDTH-250, 1150, player_health, p_h)
      File "c:/Users/ala20/Desktop/programming nea/bullet hell/bu.py", line 39, in draw_health
        for i in range(health):
    TypeError: 'pygame.Surface' object cannot be interpreted as an integer

--- bu.py
+++ bu.py
@@ -7,6 +7,7 @@ from os import path
 
 img_dir=path.join(path.dirname(__file__),'sprites')
 sound_dir=path.join(path.dirname(__file__),'sounds')
+exp_dir=path.join(path.dirname(__file__),'sprites/explos')
 
 WIDTH = 600
 HEIGHT = 1200
@@ -21,9 +22,6 @@ YELLOW = (255, 255, 0)
 
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load('Neon Dreams.mp3')
-pygame.music.play(-1)
-pygame.music.set_volume(0.5)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bullet Hell")
 clock = pygame.time.Clock()
@@ -37,6 +35,14 @@ def draw_text(surf, text, size, x, y):
     text_rect.midtop=(x,y)
     surf.blit(text_surface, text_rect)
 
+#def draw_health(surf,x,y,health,img):
+    #for i in range(health):
+        #img_rect=img.get_rect()
+        #img_rect.x= x+30*i
+        #img_rect.y= y
+        #surf.blit(img, img_rect)
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -48,16 +54,21 @@ class Player(pygame.sprite.Sprite):
         self.speedy = 0
         self.speedx = 0
         self.radius=25
+        self.health=5
+        self.shoot_delay=100
+        self.last_shot=pygame.time.get_ticks()
     
 
     def update(self):
         self.speedx = 0
         self.speedy = 0
         keypress = pygame.key.get_pressed()
+        if keypress[pygame.K_SPACE]:
+            self.shoot()
         if keypress[pygame.K_LEFT] or keypress[pygame.K_a]:
-            self.speedx = -8
+            self.speedx = -10
         if keypress[pygame.K_RIGHT] or keypress[pygame.K_d]:
-            self.speedx = 8
+            self.speedx = 10
         if keypress[pygame.K_DOWN] or keypress[pygame.K_s]:
             self.speedy = 5
         if keypress[pygame.K_UP] or keypress[pygame.K_w]:
@@ -72,11 +83,13 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom = HEIGHT
 
     def shoot(self):
-        bullet=Bullet(self.rect.centerx,self.rect.top)
-        all_sprites.add(bullet)
-        bullets.add(bullet)
-        player_shoot.play()
-
+        state=pygame.time.get_ticks()
+        if state- self.last_shot>self.shoot_delay:
+            self.last_shot=state
+            bullet=Bullet(self.rect.centerx,self.rect.top)
+            all_sprites.add(bullet)
+            bullets.add(bullet)
+            player_shoot.play()
 
     def bomb(self):
         pass
@@ -90,6 +103,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.bottom=y
         self.rect.centerx=x
         self.speedy=-10
+        self.dmg=1
 
     def update(self):
         self.rect.y+=self.speedy
@@ -153,6 +167,7 @@ class BigMob(pygame.sprite.Sprite):
         self.rect.y = random.randrange(-250, -150)
         self.speedy = random.randrange(4, 5)
         self.radius=45
+        self.health=4
 
     def shoot(self):
         bigmob_bullet=BigMobBullet(self.rect.centerx,self.rect.bottom)
@@ -176,6 +191,7 @@ class BigMobBullet(pygame.sprite.Sprite):
         self.rect.bottom=y
         self.rect.centerx=x
         self.speedy= 6
+        self.radius=20
 
     def update(self):
         self.rect.y+=self.speedy
@@ -191,6 +207,7 @@ class Boss(pygame.sprite.Sprite):
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-250, -150)
         self.speedy = random.randrange(1, 2)
+        self.health=50
 
     def update(self):
         self.rect.y += self.speedy
@@ -200,23 +217,31 @@ class Boss(pygame.sprite.Sprite):
             self.speedy = random.randrange(1, 2)
 
 
-
 #load music
 #mysound = pygame.mixer.Sound("mysound.wav")
 #mysound.set_volume(0.5)
-bg_music=pygame.mixer.Music.load(path.join(sound_dir, 'Neon Dreams.mp3'))
-
+pygame.mixer.music.load(path.join(sound_dir, 'Neon Dreams.mp3'))
+pygame.mixer.music.set_volume(0.1)
 player_shoot=pygame.mixer.Sound(path.join(sound_dir, 'player shoot.wav'))
+player_shoot.set_volume(0.2)
 mob_shoot=pygame.mixer.Sound(path.join(sound_dir, 'mob shoot.wav'))
+mob_shoot.set_volume(0.2)
 bigmob_shoot=pygame.mixer.Sound(path.join(sound_dir, 'bigmob shoot.wav'))
+bigmob_shoot.set_volume(0.2)
 
 player_kill=pygame.mixer.Sound(path.join(sound_dir, 'player kill.wav'))
+player_kill.set_volume(0.2)
 mob_kill=pygame.mixer.Sound(path.join(sound_dir, 'mob kill.wav'))
+mob_kill.set_volume(0.2)
 bigmob_kill=pygame.mixer.Sound(path.join(sound_dir, 'bigmob kill.wav'))
+bigmob_kill.set_volume(0.2)
 
 
 #load game img
 player_img=pygame.image.load(path.join(img_dir, 'p1.png')).convert_alpha ()
+player_health=pygame.image.load(path.join(img_dir, 'player_health.png')).convert_alpha()
+p_h=pygame.transform.scale(player_health, (50,50))
+
 bullet_img=pygame.image.load(path.join(img_dir, 'b1.png')).convert_alpha ()
 mobbullet_img=pygame.image.load(path.join(img_dir, 'bm1.png')).convert_alpha ()
 bigmob_bullet_img=pygame.image.load(path.join(img_dir, 'bm2.png')).convert_alpha ()
@@ -224,6 +249,9 @@ mob_img=pygame.image.load(path.join(img_dir, 'mob.png')).convert_alpha ()
 bigmob_img=pygame.image.load(path.join(img_dir, 'bigmob.png')).convert_alpha ()
 boss_img=pygame.image.load(path.join(img_dir, 'boss.png')).convert_alpha ()
 
+
+
+######################################################################################
 all_sprites = pygame.sprite.Group()
 bullets=pygame.sprite.Group()
 mob_bullets=pygame.sprite.Group()
@@ -234,11 +262,11 @@ boss=pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 
-for i in range(4):
+for i in range(8):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
-for i in range (2):
+for i in range (4):
     bm=BigMob()
     all_sprites.add(bm)
     bigmobs.add(bm)
@@ -247,20 +275,37 @@ for i in range (1):
     all_sprites.add(b)
     boss.add(b)
 
+pygame.mixer.music.play(-1)
+
 score=0
+
+def mob_respawn():
+    mob_kill.play()
+    print ('score=',score )
+    m=Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+def bigmob_respawn():
+    bigmob_kill.play()
+    bm=BigMob()
+    all_sprites.add(bm)
+    bigmobs.add(bm)
+
+def boss_respawn():
+    b=Boss()
+    all_sprites.add(b)
+    boss.add(b)
+
 
 # Game loop
 running = True
 while running:
     # keep loop running at the right speed
-    bg_music.play(-1)
     clock.tick(FPS)
     # Process input (events)
     for event in pygame.event.get():
         # check for closing window
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_SPACE:
-                player.shoot()
         if event.type == pygame.QUIT:
             running = False
     #make mobs shoot + player and bullets collide
@@ -269,16 +314,20 @@ while running:
         if random.randrange(adj_odds)==0:
             mob.shoot()                                                    #improved circular collision
     mob_bullet_hits=pygame.sprite.spritecollide(player,mob_bullets, True, pygame.sprite.collide_circle)
-    if mob_bullet_hits:
-        running=False
-    
+    for mob_bullet_hit in mob_bullet_hits:
+        player.health-=1
+        if player.health<=0:
+            running=False
+    #bullet frequency
     for bigmob in bigmobs:
         adj_odds=int(50*1/10)
         if random.randrange(adj_odds)==0:
             bigmob.shoot()       
-    bigmob_bullet_hits=pygame.sprite.spritecollide(player,bigmob_bullets, True,)
-    if bigmob_bullet_hits:
-        running=False
+    bigmob_bullet_hits=pygame.sprite.spritecollide(player,bigmob_bullets, True,pygame.sprite.collide_circle)
+    for bigmob_bullet_hit in bigmob_bullet_hits:
+        player.health-=1
+        if player.health<=0:
+            running=False
     
 
     # Update
@@ -286,45 +335,41 @@ while running:
     #check if mob collided with bullet
     mobhits=pygame.sprite.groupcollide(mobs, bullets, True, True)
     for mobhit in mobhits:
-        mob_kill.play()
         score+=100
-        print ('score=',score )
-        m=Mob()
-        all_sprites.add(m)
-        mobs.add(m)
+        mob_respawn()
     bigmobhits=pygame.sprite.groupcollide(bigmobs, bullets, True, True)
     for bigmobhit in bigmobhits:
-        bigmob_kill.play()
         score+=750
-        bm=BigMob()
-        all_sprites.add(bm)
-        bigmobs.add(bm)
+        bigmob_respawn()
     bosshits=pygame.sprite.groupcollide(boss, bullets, True, True)
     for bosshit in bosshits:
-        score+=1500
-        b=Boss()
-        all_sprites.add(b)
-        boss.add(b)
+        score+=1500 
+        boss_respawn()
     
     #check for collisions
-    mobhits=pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
-    if mobhits:
-        running=False
-    bigmobhits=pygame.sprite.spritecollide(player, bigmobs, False, pygame.sprite.collide_circle)
-    if bigmobhits:
-        running=False
+    mobhits=pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
+    for mobhit in mobhits:
+        player.health-=1
+        if player.health<=0:
+            running=False
+    bigmobhits=pygame.sprite.spritecollide(player, bigmobs, True, pygame.sprite.collide_circle)
+    for bigmobhit in bigmobhits:
+        player.health-=1
+        if player.health<=0:
+            running=False
     bosshits=pygame.sprite.spritecollide(player, boss, False)
     if bosshits:
         running=False
+
     #coolide mobs and kill if they get too close to each other
 
     # Draw / render
     screen.fill(BLACK)
     all_sprites.draw(screen)
     # *after* drawing everything, flip the display
+    #draw_health(screen, WIDTH-250, 1150, player_health, p_h)
     draw_text(screen, str(score), 50, WIDTH/2,50)
     pygame.display.flip()
-    pygame.display.update()
 
 pygame.quit()
-  
+    
